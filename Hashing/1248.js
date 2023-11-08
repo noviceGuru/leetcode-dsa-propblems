19183fc72696705f8906f9e72d9d30bb6a8fa6b8
noviceGuru <amin.arabi@gmail.com>
2023-11-08 06:11:22 +0100

    1248 solved

--- Hashing/1248.js
+++ Hashing/1248.js
@@ -12,37 +12,22 @@ const data = [
         k: 2
     }
 ]
+let toApply = data[0]
 
 const numberOfSubarraysWithKDistinct = (nums, k) => {
     let ans = 0
-    let left = 0
-    let right = 0
-    let count = (nums[0] % 2 !== 0) ? 1 : 0
+    let curr = 0
+    let ansCounts = new Map()
+    ansCounts.set(0, 1)
 
-    while (right < nums.length && left < nums.length) {
-        if (count < k && nums[right + 1]) {
-            right++
-            if (!isNaN(nums[right]) && nums[right] % 2 !== 0) {
-                count++
-            }
-        } else if (left < nums.length) {
-            if (nums[left] % 2 !== 0) {
-                count--
-            }
-            left++
-        }
-
-        if (count === k) {
-            ans++
-        }
-
-        console.log(count === k, nums.slice(left, right + 1), 'left', left, 'right', right, 'count', count, 'ans', ans)
+    for (const num of nums) {
+        curr += num%2
+        ans += ansCounts.get(curr - k) || 0
+        ansCounts.set(curr, (ansCounts.get(curr) || 0) +1)
     }
-
     return ans
 }
 
-let toApply = data[2]
 console.log(numberOfSubarraysWithKDistinct(toApply.nums, toApply.k))
 
 // NOT POSSIBLE WITH SLIDING WINDOW AT ALL
