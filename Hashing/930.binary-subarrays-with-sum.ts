051b7e4ab7693e58bdb8ee8992e8ee582813bccb
noviceGuru <amin.arabi@gmail.com>
2023-11-13 08:00:26 +0100

    930.binary-subarrays-with-sum.ts improved

--- Hashing/930.binary-subarrays-with-sum.ts
+++ Hashing/930.binary-subarrays-with-sum.ts
@@ -8,39 +8,19 @@ const nums = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
 const goal = 0
 
 function numSubarraysWithSum(nums: number[], goal: number): number {
-    let ans = 0
+    let result = 0
+    
+    let currSum = 0
+    const sumsMap = new Map()
+    sumsMap.set(0, 1)
 
-    let prefixSum = new Map<number, number>()
-    prefixSum.set(0, 1)
-    let current = 0
-
-    nums.forEach(e => {
-        current += e
-        if (prefixSum.has(current)) {
-            //@ts-ignore
-            prefixSum.set(current, prefixSum.get(current) + 1)
-        } else {
-            prefixSum.set(current, 1)
-        }
+    nums.forEach(num => {
+        currSum += num
+        result += sumsMap.get(currSum - goal) ?? 0
+        sumsMap.set(currSum, (sumsMap.get(currSum) ?? 0) + 1)
     })
 
-    console.log(prefixSum)
-
-    if (goal === 0) {
-        prefixSum.forEach(e => {
-            let n = e - 1
-            ans += (n * (n + 1)) / 2
-        })
-    } else {
-        prefixSum.forEach((value, key) => {
-            if (prefixSum.has(key - goal)) {
-                //@ts-ignore
-                ans += value * prefixSum.get(key - goal)
-            }
-        })
-    }
-
-    return ans
+    return result
 }
 
 console.log("ans", numSubarraysWithSum(nums, goal))
