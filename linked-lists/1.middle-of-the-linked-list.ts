57471d9f2912cc2106466a9afaad5aafb2a2596c
noviceGuru <amin.arabi@gmail.com>
2023-11-24 08:09:59 +0100

    1.middle-of-the-linked-list.ts solved

---
+++ linked-lists/1.middle-of-the-linked-list.ts
@@ -0,0 +1,11 @@
+const middleNode = (head: ListNode | null): ListNode | null => {
+    let slow = head
+    let fast = head
+
+    while(fast && fast.next){
+        slow = slow?.next
+        fast = fast.next.next
+    }
+
+    return slow
+}
