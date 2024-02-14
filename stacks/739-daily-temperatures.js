f2980e412563c03c6aab5c09d0a51bc7eb5e164d
noviceGuru <amin.arabi@gmail.com>
2024-02-14 07:44:20 +0100

    739-daily-temperatures.js solved correctly

--- stacks/739-daily-temperatures.js
+++ stacks/739-daily-temperatures.js
@@ -3,20 +3,12 @@ function dailyTemperatures(temps) {
     let stack = [[temps[0], 0]]
     // [t, index]
     for (let i = 1; i < temps.length; i++) {
-        if(temps[i]<(stack[stack.length -1] || [1000 , 0])[0]){
-            stack.push([temps[i], i])
-        }else{
-            // keep poping till it's true
-            while((stack[stack.length -1] || [1000,0][0]) <= temps[i]){
-                ans[stack[stack.length -1][1]] = stack[stack.length -1][1] -i
-                stack.pop()
-
-                if((stack[stack.length -1] || [1000,0][0]) > temps[i]){
-                    stack.push([temps[i], i])
-                }
-            }
+        // keep poping till it's true
+        while ((stack[stack.length - 1] || [1000, 0])[0] < temps[i]) {
+            ans[stack[stack.length - 1][1]] = i - stack[stack.length - 1][1]
+            stack.pop()
         }
-
+        stack.push([temps[i], i])
     }
 
     console.log(stack)
@@ -24,5 +16,6 @@ function dailyTemperatures(temps) {
 }
 
 const input1 = [73, 74, 75, 71, 69, 72, 76, 73]
+const input2 = [89, 62, 70, 58, 47, 47, 46, 76, 100, 70]
 
 console.log(dailyTemperatures(input1))
